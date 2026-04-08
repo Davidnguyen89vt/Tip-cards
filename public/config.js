840a21d6628efa9d4eec2a8a1b851dc387188936
David Nguyen <david_n_nguyen@hotmail.com>
2026-04-08 13:12:25 -0400

    Use local technicians CSV for live tip page

--- public/config.js
+++ public/config.js
@@ -1,4 +1,3 @@
 window.APP_CONFIG = {
-  googleSheetCsvUrl:
-    'https://docs.google.com/spreadsheets/d/e/2PACX-1vS7_3XcyHhHnh-39XNYHOcTgR57yNXn2xe-IYD59N-C4PH8wkl4_BNVWP5jNBuIOeR-1BcWkIEfSME5/pub?gid=0&single=true&output=csv'
+  googleSheetCsvUrl: 'technicians.csv'
 };
